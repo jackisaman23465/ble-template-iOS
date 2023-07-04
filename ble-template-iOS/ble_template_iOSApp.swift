52eb0f356f00db695f413af5a47c1f388943c526
jackisaman23465 <72491009+jackisaman23465@users.noreply.github.com>
2023-07-04 10:05:59 +0800

    Initial Commit

---
+++ ble-template-iOS/ble_template_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ble_template_iOSApp.swift
+//  ble-template-iOS
+//
+//  Created by 周子傑 on 2023/7/4.
+//
+
+import SwiftUI
+
+@main
+struct ble_template_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
